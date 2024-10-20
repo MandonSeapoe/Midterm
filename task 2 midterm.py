df36621b87867b49e91ca2abb0410f6f8d1c3563
MandonSeapoe <mseapoe1@gmail.com>
2024-10-20 12:22:52 -0400

    Add files via upload

---
+++ task 2 midterm.py
@@ -0,0 +1,11 @@
+START
+   1. Input the number (n)
+   2. Set factorial = 1
+   3. If n is 0 or 1
+        - Output "Factorial is 1"
+      Else
+        - For i from 2 to n
+             Multiply factorial by i
+        - End loop
+   4. Output the factorial
+END
